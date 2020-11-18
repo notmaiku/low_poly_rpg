660004bb9d1176629144d1ebb2f738044a4a0160
Jay <jbruce3@masonlive.gmu.edu>
2020-11-18 12:38:46 -0500

    fixed explosion to not multiply

--- Assets/Bullet_Collide.cs
+++ Assets/Bullet_Collide.cs
@@ -6,29 +6,32 @@ public class Bullet_Collide : MonoBehaviour {
   public Rigidbody explosion;
   public float radius = 100.0F;
   public float power = 100.0F;
+  private bool exploded=false;
   void OnCollisionEnter(Collision collision) {
-    try {
+    /*try {
         EnemyController enemy = collision.collider.GetComponent("EnemyController") as EnemyController;
         enemy.HP -= 25;
         print(enemy.HP);
     } catch (System.NullReferenceException e) {
         print("ERROR: Bullet collided with a non enemy - " + e);
+    }*/
+    if(!exploded)
+    {
+        exploded=true;
+        Vector3 explosionPos = transform.position;
+        Rigidbody bomb = Instantiate(explosion, transform.position, transform.rotation);
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders) {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            try {
+                EnemyController enemy = collision.collider.GetComponent("EnemyController") as EnemyController;
+                enemy.HP -= 50;
+                print(enemy.HP);
+            } catch (System.NullReferenceException e) {
+            print("ERROR: Bullet collided with a non enemy - " + e);
+            }
+        }
     }
-
-    // Vector3 explosionPos = transform.position;
-    // Rigidbody bomb;
-    // bomb = Instantiate(explosion, transform.position, transform.rotation);
-    // Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-    // foreach (Collider hit in colliders) {
-    //     Rigidbody rb = hit.GetComponent<Rigidbody>();
-    //     try {
-    //         EnemyController enemy = collision.collider.GetComponent("EnemyController") as EnemyController;
-    //         enemy.HP -= 50;
-    //         print(enemy.HP);
-    //     } catch (System.NullReferenceException e) {
-    //     print("ERROR: Bullet collided with a non enemy - " + e);
-    //     }
-    // }
   }
 }
 // void OnDrawGizmos() {
